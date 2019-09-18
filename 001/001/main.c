ff94c60db66e58a6d1bdb0270ea662f5fec0ee67
root <root@502-12.localdomain>
2019-09-18 13:07:09 +0900

    001_Blinky_led_basic

---
+++ 001/001/main.c
@@ -0,0 +1,42 @@
+﻿
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+
+	#define F_CPU	16000000UL
+	#include <avr/io.h>
+	#include <util/delay.h>
+
+	void init_led(void)
+	{
+		DDRC = 0x01;
+		PORTC = 0x00;
+	}
+
+	void led_on(void)
+	{
+		PORTC = 0x01;
+	}
+
+	void led_off(void)
+	{
+		PORTC = 0x00;
+	}
+
+	int main(void)
+	{
+		init_led();
+		
+		while (1)
+		{
+			led_on();
+			_delay_ms(500);
+			led_off();
+			_delay_ms(500);
+		}
+		return 0;
+	}
+}
